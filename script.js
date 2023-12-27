7cbc9d8fb08835bbe4dbaf9c5d8bc260c0e983c2
ThePratham28 <112806898+ThePratham28@users.noreply.github.com>
2023-12-27 19:39:17 +0530

    js bug fixes

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 async function fetchWeather() {
+
     let searchInput = document.getElementById("search").value;
-    const weatherDataSelection = document.getElementById("weather-data");
-    weatherDataSelection.style.display = "block";
-    const apiKey = "38bd384f26f83d20d802e65b67cf4e5e"
+    const weatherDataSection = document.getElementById("weather-data");
+    weatherDataSection.style.display = "block";
+    
+    const apiKey = "38bd384f26f83d20d802e65b67cf4e5e";
 
     if(searchInput == "") {
         weatherDataSection.innerHTML = `
@@ -24,6 +26,7 @@ async function fetchWeather() {
           console.log("Bad response! ", response.status);
           return;
         }
+
         const data = await response.json();
         if(data.length == 0) {
             console.log("Something went wrong here.");
@@ -41,18 +44,24 @@ async function fetchWeather() {
     }
     async function getWeatherData(lon, lat) {
         const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
+        
         const response = await fetch(weatherURL);
+        
+        if(!response.ok) {
+            console.log("Bad response! ", response.status);
+            return;
+        }
 
         const data = await response.json();
-    weatherDataSection.style.display = "flex";
-    weatherDataSection.innerHTML = `
-    <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="${data.weather[0].description}" width="100" />
-    <div>
-      <h2>${data.name}</h2>
-      <p><strong>Temperature:</strong> ${Math.round(data.main.temp - 273.15)}°C</p>
-      <p><strong>Description:</strong> ${data.weather[0].description}</p>
-    </div>
-    `
+        weatherDataSection.style.display = "flex";
+        weatherDataSection.innerHTML = `
+        <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="${data.weather[0].description}" width="100" />
+        <div>
+        <h2>${data.name}</h2>
+        <p><strong>Temperature:</strong> ${Math.round(data.main.temp - 273.15)}°C</p>
+        <p><strong>Description:</strong> ${data.weather[0].description}</p>
+        </div>
+        `
     }
 
     document.getElementById("search").value = "";
